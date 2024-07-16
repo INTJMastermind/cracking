f8e03f131f89d5ed899b20523a57fc2447b0955a
INTJMastermind <cleverusername@gmail.com>
2024-07-15 18:28:40 -0700

    Refactored islands.py to accept non-square grids, also moved checking out of bounds from the neighbors function to the check function

--- islands.py
+++ islands.py
@@ -1,66 +1,54 @@
 # Compute the number of islands in a grid
-import random
+#import random
 
-def makeGrid(size):
+def makeGrid(numRows, numFiles):
+    import random
     grid = []
-    for _x in range(0, size):
+    for _x in range(0, numRows):
         row = []
-        for _y in range(0, size):
+        for _y in range(0, numFiles):
             row.append(random.randint(0,1))
         grid.append(row)
     return grid
 
-def getNeighbors(coord, size):
-    # Returns a list of coordinates of the neighbors of a square. This includes diagonal neighbors.
+def getNeighbors8(coord):
+    # Returns a set of coordinates of the neighbors of a square. This includes diagonal neighbors.
     row = coord[0]
     file = coord[1] 
 
     possibleRows = (row-1, row, row+1)
     possibleFiles = (file-1, file, file+1)
 
-    # Filter out the edge cases...
-    filteredRows = []
-    filteredFiles = []
-
+    # Get all permutations of filteredRows and filteredFiles.
+    neighbors = set()
     for r in possibleRows:
-        if r >= 0 and r < size:
-            filteredRows.append(r)
+        for f in possibleFiles:
+            neighbors.add((r, f))
 
-    for f in possibleFiles:
-        if f >= 0 and f < size:
-            filteredFiles.append(f)
-    
-    # Get all permutations of filteredRows and filteredFiles (except for the original square)
-    neighbors = []
-
-    for r in filteredRows:
-        for f in filteredFiles:
-            if (r, f) != (row, file):   #exclude the original square
-                neighbors.append((r, f))
+    neighbors.remove((row, file)) # Remove the original square
 
     return(neighbors)
 
-def getNeighbors2(coord, size):
-    # Returns a list of coordinates of the neighbors of a square.
+
+def getNeighbors4(coord):
+    # Returns a set of coordinates of the neighbors of a square.
     # This looks ONLY at squares directly above/below and left/right.
     # Diagonal neighbors don't count.
 
     row = coord[0]
     file = coord[1] 
-    possibles = [(row-1, file), (row+1, file), (row, file-1), (row, file+1)]
-    neighbors = []
+    return {(row-1, file), (row+1, file), (row, file-1), (row, file+1)}
 
-    for possible in possibles:
-        if possible[0] >= 0 and possible[0] < size and possible[1] >= 0 and possible[1] < size:
-            neighbors.append(possible)
 
-    return(neighbors)
-
-def checkGrid(coord, grid):
-    # Checks the map grid at a coordinate.
+def checkGrid(coord, grid, numRows, numFiles):
+    # Checks the map grid at a coordinate. Returns a value of '0' if the coordinate is outside the bounds of the grid.
     row = coord[0]
     file = coord[1] 
-    return(grid[row][file])
+
+    if row < 0 or row >= numRows or file < 0 or file >= numFiles:
+        return 0
+    else:
+        return(grid[row][file])
 
 def updateGrid(coord, grid):
     # Sets the grid's value at coord to "2", which indicates 
@@ -75,30 +63,34 @@ def drawGrid(grid):
     for row in grid: print(row)
     print("")
 
-def markIsland(coord, grid, size):
+def markIsland(coord, grid, numRows, numFiles):
     # Checks if neighbors of grid are land tiles ("1"), and if so, marks them as part
     # of an existing island ("2"), and recursively checks if *their* neighbors are also land tiles.
-    if checkGrid(coord, grid) == 1:
+    # Note: We're passing numRows and numFiles because they're constants and not recalculating each time saves time.
+    if checkGrid(coord, grid, numRows, numFiles) == 1:
         updateGrid(coord, grid)
-        #neighbors = getNeighbors(coord, size) # This version looks at 8 neighbors per square (diagonals included)
-        neighbors = getNeighbors2(coord, size) # This version looks at 4 neighbors per square (diagonals not included)
+        #neighbors = getNeighbors8(coord) # This version looks at 8 neighbors per square (diagonals included)
+        neighbors = getNeighbors4(coord) # This version looks at 4 neighbors per square (diagonals not included)
         for neighbor in neighbors:
-            markIsland(neighbor, grid, size) # Recursively looks at the neighbors of that square to see if they're also land tiles.
+            markIsland(neighbor, grid, numRows, numFiles) # Recursively looks at the neighbors of that square to see if they're also land tiles.
     return
 
-def islands(size):
-    grid = makeGrid(size)
-    drawGrid(grid)
+def islands(grid):
+    # Defines the number of islands in a grid.
+    numRows = len(grid)
+    numFiles = len(grid[0])
     numIslands = 0
-    for x, row in enumerate(grid):
+    for x, row in enumerate(grid): # Move through the grid and...
         for y, value in enumerate(row):
-            if value == 1:  # If the tile at (x,y) is a land tile that's not part of an existing island...
-                numIslands += 1
+            if value == 1:  # ...if we encounter a tile that is a land tile that's not part of an existing island ('1')...
+                numIslands += 1 # ...increment the number of islands and...
                 coord = (x, y)
-                markIsland(coord, grid, size) # Recursively mark off all tiles that are part of that island.
+                markIsland(coord, grid, numRows, numFiles) # ...recursively mark off all tiles that are part of that island.
 
     print(f"Number of islands is: {numIslands}")
     return numIslands
 
 if __name__ == "__main__":
-    islands(10)
+    grid = makeGrid(4, 5)
+    drawGrid(grid)
+    islands(grid)
