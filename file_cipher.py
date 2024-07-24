f9dade531102d30f0508df433df15608be0f1477
INTJMastermind <cleverusername@gmail.com>
2024-07-24 15:29:46 -0700

    file_cipher

---
+++ file_cipher.py
@@ -0,0 +1,50 @@
+import time
+import os
+import sys
+import transposition_encrypt
+import transposition_decrypt
+
+def main():
+    input_filename = "frankenstein_encrypted.txt"
+    output_filename = "frankenstein_decrypted.txt"
+
+    my_key = 10
+    my_mode = 'decrypt' # Set to 'encrypt' or 'decrypt'
+
+    # If the input file does not exist, terminate the program early.
+    if not os.path.exists(input_filename):
+        print(f'The file {input_filename} does not exist. Quitting...')
+        sys.exit()
+
+    # If output file already exists, give the user a chance to quit.
+    if os.path.exists(output_filename):
+        print(f'This will overwrite the file {output_filename}. (C)ontinue or (Q)uit?')
+        response = input('> ')
+        if not response.lower().startswith('c'):
+            sys.exit()
+    
+    # Read in the message from the input file:
+    file_obj = open(input_filename)
+    content = file_obj.read()
+    file_obj.close()
+
+    print(f'{my_mode}ing...')
+
+    #Measure how long the encryption / decryption takes:
+    start_time = time.time()
+    if my_mode == 'encrypt':
+        translated = transposition_encrypt.encrypt_message(my_key, content)
+    elif my_mode == 'decrypt':
+        translated = transposition_decrypt.decrypt_message(my_key, content)
+    total_time = round(time.time() - start_time, 2)
+    print(f'{my_mode}ion time: {total_time} seconds.')
+
+    output_file_obj = open(output_filename, 'w')
+    output_file_obj.write(translated)
+    output_file_obj.close()
+
+    print(f'Done {my_mode}ing {input_filename} ({len(content)} characters).')
+    print(f'{my_key}ed file is {output_filename}')
+
+if __name__ == '__main__':
+    main()
